d6a979e8739ff5c9dca1c588307a15733a9317eb
Cris <c.cheuquel01@ufromail.cl>
2024-04-02 10:04:36 -0300

    ediciones menores localClient.py

--- client/localClient.py
+++ client/localClient.py
@@ -42,7 +42,7 @@ class LocalClient:
         return self.get(path)
 
     def add_todo(self, title, description):
-        task = {"title": title, "description": description}
+        task = {"user": self.user, "title": title, "description": description}
         path = f"/{self.user}/todos"
         return self.post(path, task)
 
@@ -61,14 +61,17 @@ client = LocalClient()
 # Iniciar sesión
 login_response = client.login("0000", "1234")
 print(login_response)
-
+new_user = {"rut": "1111", "password": "4321"}
+register_response = client.register(new_user["rut"], new_user["password"])
+print(register_response)
 # Obtener todas las tareas del usuario
-# todos = client.get_todos()
-# print(todos)
+#todos = client.get_todos()
+#print(todos)
 
 # Agregar una nueva tarea
-#add_response = client.add_todo("Nueva tarea", "Descripción de la nueva tarea")
-#print(add_response)
+task = {"title": "Nueva tarea", "description": "Descripción de la nueva tarea"}
+add_response = client.add_todo(task["title"], task["description"])
+print(add_response)
 
 # Cerrar sesión
 logout_response = client.logout()
